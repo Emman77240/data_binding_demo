cdeccfe36e3cdb18d24030988414b7d161cfbe27
Emmanuel Asiegbu <asiegbuemmanuelekee@gmail.com>
2024-12-16 13:09:37 +0000

    feat: Created a person model, and implemented two way data binding

--- app/src/main/java/com/northcoders/databindingdemo/MainActivity.java
+++ app/src/main/java/com/northcoders/databindingdemo/MainActivity.java
@@ -4,21 +4,29 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.northcoders.databindingdemo.databinding.ActivityMainBinding;
+import com.northcoders.databindingdemo.model.Person;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Instantiate data binding class
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Instantiate Person class
+        Person person = new Person("Emmanuel", "30", "asiegbuemmanuelekee@gmail.com");
+
+        // Bind activity by setting activity binding class to data binding util class
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // Set data object to activity main
+        activityMainBinding.setPerson(person);
     }
 }
--- app/src/main/java/com/northcoders/databindingdemo/model/Person.java
+++ app/src/main/java/com/northcoders/databindingdemo/model/Person.java
@@ -1,6 +1,11 @@
 package com.northcoders.databindingdemo.model;
 
-public class Person {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.northcoders.databindingdemo.BR;
+
+public class Person extends BaseObservable {
     private String name;
     private String age;
     private String email;
@@ -11,27 +16,34 @@ public class Person {
         this.name = name;
     }
 
+
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
     }
 
+    @Bindable
     public String getAge() {
         return age;
     }
 
     public void setAge(String age) {
         this.age = age;
+        notifyPropertyChanged(BR.age);
     }
 }
